518e7ecf8d269e27f3280277367f2dd623a245ae
NuriddinBro <nuriddinxushboqov03@gmail.com>
2023-03-06 21:17:57 +0500

    config-server

---
+++ config-server/src/main/java/uz/najot/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package uz.najot.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
---
+++ config-server/src/test/java/uz/najot/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.najot.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
