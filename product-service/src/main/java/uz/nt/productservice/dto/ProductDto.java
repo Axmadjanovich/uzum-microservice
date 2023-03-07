6c6d0ae1e33c7dea665e1cf17d93b0b7c361ee7e
MahmudSaidxonov <saidxonovmaxmudjon@gmail.com>
2023-03-07 16:27:48 +0500

    config server

--- product-service/src/main/java/uz/nt/productservice/dto/ProductDto.java
+++ product-service/src/main/java/uz/nt/productservice/dto/ProductDto.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+//spr star conf serv, spring cla str
+
+//@Enabling
 
 @Getter
 @Setter
