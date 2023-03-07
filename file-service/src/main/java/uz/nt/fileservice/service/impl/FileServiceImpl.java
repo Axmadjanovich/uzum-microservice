99b21984762ffd04960d94dc8ee4491040f4618d
Diyorbek <diorakhmadaliev@gmail.com>
2023-03-07 14:29:22 +0500

    Correction of file-service pom.

--- file-service/src/main/java/uz/nt/fileservice/service/impl/FileServiceImpl.java
+++ file-service/src/main/java/uz/nt/fileservice/service/impl/FileServiceImpl.java
@@ -5,18 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import uz.nt.fileservice.dto.FileDto;
 import uz.nt.fileservice.model.File;
 import uz.nt.fileservice.repository.FileRepository;
 import uz.nt.fileservice.service.Fileservices;
-
-import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -25,7 +23,7 @@ public class FileServiceImpl implements Fileservices {
 
     private final FileRepository fileRepository;
     @Override
-    public ResponseDto<String> fileUpload(MultipartFile file) {
+    public ResponseDto<Integer> fileUpload(MultipartFile file) {
         File fileEntity = new File();
         fileEntity.setExt(file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")));
         fileEntity.setCreatedAt(LocalDateTime.now());
@@ -34,28 +32,29 @@ public class FileServiceImpl implements Fileservices {
             String filePath;
             Files.copy(file.getInputStream(), Path.of(filePath = filePath("upload",fileEntity.getExt())));
             fileEntity.setPath(filePath);
-            fileRepository.save(fileEntity);
+            File savedFile = fileRepository.save(fileEntity);
 
-            return ResponseDto.<String>builder()
-                    .data(filePath)
+            return ResponseDto.<Integer>builder()
+                    .data(savedFile.getId())
                     .message("OK")
                     .success(true)
                     .build();
         } catch (IOException e) {
             log.error("Error while saving file: {}", e.getMessage());
-            return ResponseDto.<String>builder()
+            return ResponseDto.<Integer>builder()
                     .code(2)
                     .message("Error while saving file: " + e.getMessage())
                     .build();
         }
     }
-    public static synchronized String filePath(String folder,String ext){
+    public static String filePath(String folder,String ext){
         LocalDate localDate = LocalDate.now();
         String path = localDate.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
         java.io.File file = new java.io.File(folder + "/"+ path);
         if (!file.exists()){
             file.mkdirs();
         }
-        return file.getPath() + "\\"+ System.currentTimeMillis() + ext;
+        String uuid = UUID.randomUUID().toString();
+        return file.getPath() + "\\"+ uuid + ext;
     }
 }
