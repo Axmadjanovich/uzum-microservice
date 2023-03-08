3ea79140b26bceb658634c86e82ca69774aa19b2
MahmudSaidxonov <saidxonovmaxmudjon@gmail.com>
2023-03-09 00:52:03 +0500

    file - feign

--- file-service/src/main/java/uz/nt/fileservice/rest/FileResources.java
+++ file-service/src/main/java/uz/nt/fileservice/rest/FileResources.java
@@ -2,10 +2,7 @@ package uz.nt.fileservice.rest;
 
 import dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uz.nt.fileservice.service.Fileservices;
 
@@ -17,7 +14,7 @@ public class FileResources {
     private final Fileservices fileservices;
 
     @PostMapping
-    public ResponseDto<Integer> uploadFile(@RequestBody MultipartFile file){
+    public ResponseDto<Integer> uploadFile(@RequestPart("file") MultipartFile file){
         return fileservices.fileUpload(file);
     }
 
--- product-service/src/main/java/uz/nt/productservice/rest/ProductResources.java
+++ product-service/src/main/java/uz/nt/productservice/rest/ProductResources.java
@@ -3,16 +3,14 @@ package uz.nt.productservice.rest;
 import dto.ResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-=======
 import org.springframework.data.domain.Page;
->>>>>>> develop
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uz.nt.productservice.dto.ProductDto;
 import uz.nt.productservice.service.ProductService;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -23,59 +21,24 @@ public class ProductResources {
 
     private final ProductService productService;
 
-    @PostMapping
-    public ResponseDto<ProductDto> addNewProduct(@RequestBody ProductDto productDto) {
+    @PostMapping(consumes = {"multipart/form-data", "application/json"})
+    public ResponseDto<ProductDto> addNewProduct(@ModelAttribute ProductDto productDto) throws IOException {
         return productService.addNewProduct(productDto);
     }
 
     @PatchMapping
-<<<<<<< HEAD
-=======
 //    @PreAuthorize("hasAnyAuthority('ADMIN')")
->>>>>>> develop
     public ResponseDto<ProductDto> updateProduct(@RequestBody ProductDto productDto) {
         return productService.updateProduct(productDto);
     }
 
     @GetMapping()
-<<<<<<< HEAD
-    public ResponseDto<List<EntityModel<ProductDto>>> getAllProducts(
-            @RequestParam (defaultValue = "0") Integer page,
-            @RequestParam (defaultValue = "2") Integer size) {
-=======
     public ResponseDto<Page<EntityModel<ProductDto>>> getAllProducts(@RequestParam(defaultValue = "10") Integer size,
                                                                      @RequestParam(defaultValue = "0") Integer page) {
->>>>>>> develop
         return productService.getAllProducts(page, size);
     }
 
     @GetMapping("by-id")
-<<<<<<< HEAD
-    public ResponseDto<ProductDto> getProductById(@RequestParam Integer id, HttpServletRequest req){
-        String authorization = req.getHeader("Authorization");
-        return productService.getProductById(id);
-    }
-
-    @GetMapping("/expensive-by-category")
-    public ResponseDto<List<ProductDto>> getExpensiveProducts(){
-        return productService.getExpensiveProducts();
-    }
-
-    @GetMapping("search")
-    public ResponseDto<List<ProductDto>> universalSearch(ProductDto productDto){
-        return productService.universalSearch(productDto);
-    }
-
-    @GetMapping("search-2")
-    public ResponseDto<List<ProductDto>> universalSearch2(@RequestParam Map<String, String> params){
-        return productService.universalSearch2(params);
-    }
-
-    @GetMapping("get-sort")
-    public ResponseDto<List<ProductDto>> getAllProductWithSort(@RequestParam List<String> sort) {
-        return productService.getAllProductsWithSort(sort);
-    }
-=======
     public ResponseDto<ProductDto> getProductById(@RequestParam Integer id, HttpServletRequest req) {
         return productService.getProductById(id);
     }
@@ -101,6 +64,5 @@ public class ProductResources {
 //    public ResponseDto<List<ProductDto>> getProducts(@RequestParam List<String> sort){
 //        return productService.getAllProductsWithSort(sort);
 //    }
->>>>>>> develop
 
 }
