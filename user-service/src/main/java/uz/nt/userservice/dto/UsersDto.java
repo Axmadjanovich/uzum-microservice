868f6df2deef832cabd65076dc2798ee548187f1
Axmadjanovich <muhammadkarim@barqapp.com>
2023-03-09 20:57:17 +0500

    todo & order module

--- user-service/src/main/java/uz/nt/userservice/dto/UsersDto.java
+++ user-service/src/main/java/uz/nt/userservice/dto/UsersDto.java
@@ -1,9 +1,13 @@
 package uz.nt.userservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import validator.AppStatusMessages;
 
 import java.util.Date;
 
@@ -13,13 +17,19 @@ import java.util.Date;
 @NoArgsConstructor
 public class UsersDto {
     private Integer id;
+    @NotBlank(message = AppStatusMessages.EMPTY_STRING)
     private String phoneNumber;
+    @NotBlank(message = AppStatusMessages.EMPTY_STRING)
     private String firstName;
+    @NotBlank(message = AppStatusMessages.EMPTY_STRING)
     private String lastName;
     private String middleName;
+    @NotBlank(message = AppStatusMessages.EMPTY_STRING)
+    @Email(message = AppStatusMessages.NOT_VALID_EMAIL)
     private String email;
     private String gender;
     private Date birthDate;
+    @NotBlank(message = AppStatusMessages.EMPTY_STRING)
     private String password;
     private String role;
 }
