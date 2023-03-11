e2f8e60dd361cf249aac9ff006b98d70ba69dd55
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-03-11 21:37:43 +0500

    Add cloud-gateway

---
+++ cloud-geteway/src/main/java/uz/nt/cloudgeteway/CloudGetewayApplication.java
@@ -0,0 +1,13 @@
+package uz.nt.cloudgeteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGetewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGetewayApplication.class, args);
+    }
+
+}
---
+++ cloud-geteway/src/test/java/uz/nt/cloudgeteway/CloudGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.nt.cloudgeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGetewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
