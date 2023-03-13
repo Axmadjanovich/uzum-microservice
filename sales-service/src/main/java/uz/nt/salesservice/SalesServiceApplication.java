8abe2fdd778122ed0b335a99df5970cd42481da1
Sevdiyor <sevdiyoryoldoshev7@gmail.com>
2023-03-13 21:04:29 +0500

    Add getaway .

--- sales-service/src/main/java/uz/nt/salesservice/SalesServiceApplication.java
+++ sales-service/src/main/java/uz/nt/salesservice/SalesServiceApplication.java
@@ -2,8 +2,10 @@ package uz.nt.salesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SalesServiceApplication {
 
     public static void main(String[] args) {
