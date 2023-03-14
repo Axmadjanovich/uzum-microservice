f3bf7b3bef254265e0d47676b7dad3b0fe0d3bab
NuriddinBro <nuriddinxushboqov03@gmail.com>
2023-03-14 18:03:04 +0500

    Swagger category & units

--- product-service/src/main/java/uz/nt/productservice/rest/CategoryResources.java
+++ product-service/src/main/java/uz/nt/productservice/rest/CategoryResources.java
@@ -1,6 +1,9 @@
 package uz.nt.productservice.rest;
 
 import dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import uz.nt.productservice.dto.CategoryDto;
@@ -13,25 +16,82 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoryResources {
     private final CategoryService categoryService;
-
+    @Operation(
+            summary = "Add new Category",
+            method = "Add new Category",
+            description = "Need to send CategoryDto to this endpoint to create new category",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Category info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK")}
+    )
     @PostMapping()
     public ResponseDto<CategoryDto> addNewCategory(@RequestBody CategoryDto categoryDto){
         return categoryService.addCategory(categoryDto);
     }
 
+    @Operation(
+            summary = "Update Category",
+            method = "Update Category",
+            description = "Need to send CategoryDto to this endpoint to category",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Category info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Category not found")}
+    )
     @PatchMapping()
     public ResponseDto<CategoryDto> editCategory(@RequestBody CategoryDto categoryDto){
         return categoryService.updateCategory(categoryDto);
     }
+
+
+    @Operation(
+            summary = "Get all Categories",
+            method = "Get Category",
+            description = "Get all categories",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Category info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Category not found")}
+    )
     @GetMapping()
     public ResponseDto<List<CategoryDto>> viewAllCategory(){
         return categoryService.getAll();
     }
+
+
+
+    @Operation(
+            summary = "Get Category by id",
+            method = "Get Category by id",
+            description = "Get category by id",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "category info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+                    @ApiResponse(responseCode = "404", description = "Category not found")}
+    )
     @GetMapping("by-id")
     public ResponseDto<CategoryDto> viewById(@RequestParam Integer id){
         return categoryService.getById(id);
     }
 
+
+
+    @Operation(
+            summary = "Delete",
+            method = "Delete category",
+            description = "Delete category",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Category info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+                    @ApiResponse(responseCode = "404", description = "Category not found")}
+    )
     @DeleteMapping()
     public ResponseDto<CategoryDto> removeCategory(@RequestParam Integer id){
         return categoryService.deleteCategory(id);
--- product-service/src/main/java/uz/nt/productservice/rest/UnitResources.java
+++ product-service/src/main/java/uz/nt/productservice/rest/UnitResources.java
@@ -1,6 +1,9 @@
 package uz.nt.productservice.rest;
 
 import dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +18,60 @@ import java.util.List;
 public class UnitResources {
     private final UnitService unitService;
 
+    @Operation(
+            summary = "Add new Units",
+            method = "Add new units",
+            description = "Need to send UnitDto to this endpoint to create new unit",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Unit info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK")}
+    )
     @PostMapping
     public ResponseDto<UnitDto> addNewUnit(@RequestBody @Valid UnitDto unitDto){
         return unitService.addNewUnit(unitDto);
     }
+
+    @Operation(
+            summary = "Add new Unit",
+            method = "Add new Unit",
+            description = "Need to send UnitDto to this endpoint to create new unit",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Unit info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK")}
+    )
     @GetMapping
     public ResponseDto<List<UnitDto>> getAllUnits(){
         return unitService.getAllUnits();
     }
+
+    @Operation(
+            summary = "Update Units",
+            method = "Update Units",
+            description = "Need to send UnitDto to this endpoint to unit",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Units info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Units not found")}
+    )
     @PatchMapping
     public ResponseDto<UnitDto> updateUnit(@RequestBody UnitDto unitDto){
         return unitService.updateUnit(unitDto);
     }
+
+    @Operation(
+            summary = "Delete unit",
+            method = "Delete unit",
+            description = "Delete unit",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Unit info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+                    @ApiResponse(responseCode = "404", description = "Unit not found")}
+    )
     @DeleteMapping
     public ResponseDto<UnitDto> deleteUnit(@RequestParam  Integer id){
         return unitService.deleteUnit(id);
