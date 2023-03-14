d40e5addedb7d017d1e37a738dc5e3f72ded9b7b
Diyorbek <diorakhmadaliev@gmail.com>
2023-03-14 18:59:05 +0500

    Ready

--- file-service/src/main/java/uz/nt/fileservice/rest/ExceptionHandlerResource.java
+++ file-service/src/main/java/uz/nt/fileservice/rest/ExceptionHandlerResource.java
@@ -4,6 +4,7 @@ import dto.ResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import uz.nt.fileservice.exceptions.ExcelWriterException;
 import uz.nt.fileservice.exceptions.FileConvertingException;
 
 import static validator.AppStatusCodes.UNEXPECTED_ERROR_CODE;
@@ -22,7 +23,7 @@ public class ExceptionHandlerResource {
     }
 
     @ExceptionHandler
-    public ResponseEntity<ResponseDto<Void>> excelWriterException(FileConvertingException e) {
+    public ResponseEntity<ResponseDto<Void>> excelWriterException(ExcelWriterException e) {
         return ResponseEntity.badRequest()
                 .body(ResponseDto.<Void>builder()
                         .message(UNEXPECTED_ERROR + ": Error while writing report to excel file. " + e.getMessage())
