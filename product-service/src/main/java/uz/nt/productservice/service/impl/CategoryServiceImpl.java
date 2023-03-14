717199163056876345786db41ef69ee150ae4adb
MahmudSaidxonov <saidxonovmaxmudjon@gmail.com>
2023-03-14 17:44:46 +0500

    category...

--- product-service/src/main/java/uz/nt/productservice/service/impl/CategoryServiceImpl.java
+++ product-service/src/main/java/uz/nt/productservice/service/impl/CategoryServiceImpl.java
@@ -2,9 +2,10 @@ package uz.nt.productservice.service.impl;
 
 import dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Service;
 import uz.nt.productservice.dto.CategoryDto;
+import uz.nt.productservice.dto.ProductDto;
+import uz.nt.productservice.dto.UnitDto;
 import uz.nt.productservice.models.Category;
 import uz.nt.productservice.repository.CategoryRepository;
 import uz.nt.productservice.service.CategoryService;
@@ -12,6 +13,8 @@ import uz.nt.productservice.service.mapper.CategoryMapper;
 
 import java.util.List;
 import java.util.Optional;
+
+import static validator.AppStatusCodes.OK_CODE;
 
 @Service
 @RequiredArgsConstructor
@@ -23,16 +26,25 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public ResponseDto<CategoryDto> addCategory(CategoryDto categoryDto) {
+        try {
+            return ResponseDto.<CategoryDto>builder()
+                    .data(categoryMapper.toDto(
+                            categoryRepository.save(
+                                    categoryMapper.toEntity(categoryDto)
+                            )
+                    ))
+                    .message("OK")
+                    .success(true)
+                    .build();
 
-        return ResponseDto.<CategoryDto>builder()
-                .data(categoryMapper.toDto(
-                        categoryRepository.save(
-                                categoryMapper.toEntity(categoryDto)
-                        )
-                ))
-                .message("OK")
-                .success(true)
-                .build();    }
+        }catch (Exception e ){
+            return ResponseDto.<CategoryDto>builder()
+                    .code(-1)
+                    .message("DATABASE_ERROR" + ": " + e.getMessage())
+                    .data(categoryDto)
+                    .build();
+        }
+    }
 
     @Override
     public ResponseDto<CategoryDto> updateCategory(CategoryDto categoryDto) {
@@ -68,15 +80,56 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public ResponseDto<CategoryDto> getAll() {
+    public ResponseDto<List<CategoryDto>> getAll() {
+        List<CategoryDto> categoryList = categoryRepository.findAll().stream().map(categoryMapper::toDto).toList();
 
-        List<CategoryDto> categoryDto = categoryRepository.findAll().stream().map(categoryMapper::toDto).toList();
-
-        List<Category> categoryList = categoryRepository.findAll();
-        return ResponseDto.<CategoryDto>builder()
-                .message("OK")
-                .data((CategoryDto) categoryList)
+        if (categoryList.size() > 1) {
+            return ResponseDto.<List<CategoryDto>>builder()
+                    .message("OK")
+                    .data(categoryList)
+                    .success(true)
+                    .build();
+        }
+        return ResponseDto.<List<CategoryDto>>builder()
+                .message("Categories are not found!")
                 .success(true)
+                .build();
+    }
+
+    @Override
+    public ResponseDto<CategoryDto> getById(Integer id) {
+        return categoryRepository.findById(id)
+                .map(category -> ResponseDto.<CategoryDto>builder()
+                        .data(categoryMapper.toDto(category))
+                        .success(true)
+                        .code(0)
+                        .message("OK")
+                        .build())
+                .orElse(ResponseDto.<CategoryDto>builder()
+                        .message("NOT_FOUND")
+                        .code(-1)
+                        .build()
+                );
+    }
+
+    @Override
+    public ResponseDto<CategoryDto> deleteCategory(Integer id) {
+        Optional<Category> categoryById = categoryRepository.findById(id);
+
+        if (categoryById.isPresent()) {
+            Category category = categoryById.get();
+            categoryRepository.deleteById(id);
+            return ResponseDto.<CategoryDto>builder()
+                    .message("Category " + id + " has been deleted")
+                    .code(1)
+                    .success(true)
+                    .data(categoryMapper.toDto(category))
+                    .build();
+        }
+        return ResponseDto.<CategoryDto>builder()
+                .message("Category " + id + " is not available")
+                .success(false)
+                .code(-1)
                 .build();
     }
 }
