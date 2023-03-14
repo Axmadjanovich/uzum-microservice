724a8b53d4a2ac8d36d8ce2a5e8be3693bf15a92
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-03-14 21:47:56 +0500

    cloud gateway

--- cloud-gateway/src/main/java/uz/nt/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/uz/nt/cloudgateway/CloudGatewayApplication.java
@@ -2,8 +2,10 @@ package uz.nt.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudGatewayApplication {
 
     public static void main(String[] args) {
